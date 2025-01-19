c24f427d831bbedca1909cd1ad03d507c8d9b5be
Paul Rogalinski-Pinter <github@t00ltime.de>
2025-01-20 00:00:40 +0100

    feat: enable doubletap to bootloader

--- config.h
+++ config.h
@@ -7,3 +7,8 @@
 #define OLED_WIDTH 32
 #define OLED_HEIGHT 128
 
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
+#define QUANTUM_PAINTER_DISPLAY_TIMEOUT 5000
