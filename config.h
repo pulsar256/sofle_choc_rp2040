74b14464ed9bcf4d8b95fe30a4972b5ce4528d57
Paul Rogalinski-Pinter <github@t00ltime.de>
2025-01-20 08:38:58 +0100

    style: clean up config

--- config.h
+++ config.h
@@ -3,12 +3,14 @@
 #define I2C1_SCL_PIN GP3
 #define I2C1_SDA_PIN GP2
 #define I2C_DRIVER I2CD1
-#define OLED_BRIGHTNESS 128
-#define OLED_WIDTH 32
 #define OLED_HEIGHT 128
+#define OLED_WIDTH 32
 
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 
-#define QUANTUM_PAINTER_DISPLAY_TIMEOUT 5000
+#define QUANTUM_PAINTER_DISPLAY_TIMEOUT 300000
+
+#define RGB_MATRIX_SLEEP     // turn off effects when suspended
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100  // limits maximum brightness of LEDs (max 255). Higher may cause the controller to crash.
